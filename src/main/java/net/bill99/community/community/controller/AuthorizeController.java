b42004983ec4bc7cfd813742b14be5a040dc48a2
beijixing0202 <wangsy0202@163.com>
2020-02-18 10:49:19 +0800

    登陆修改

--- src/main/java/net/bill99/community/community/controller/AuthorizeController.java
+++ src/main/java/net/bill99/community/community/controller/AuthorizeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -30,7 +31,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientid);
         accessTokenDto.setClient_secret(clientsecret);
@@ -39,7 +41,15 @@ public class AuthorizeController {
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.print(user.getName());
-        return "index";
+        //System.out.print(user.getName());
+        if(user != null){
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";//重定向到首页
+        }else{
+            //登陆失败，重新登陆
+            return "redirect:/";
+        }
+        //return "index";
     }
 }
