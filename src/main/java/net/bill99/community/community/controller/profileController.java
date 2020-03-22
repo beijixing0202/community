6362c408410b10a4f24a83dcf21434048eee439a
beijixing0202 <wangsy0202@163.com>
2020-03-22 22:32:32 +0800

    '提交我的问题栏和登录拦截'

---
+++ src/main/java/net/bill99/community/community/controller/profileController.java
@@ -0,0 +1,9 @@
+package net.bill99.community.community.controller;
+
+/**
+ * @Author: wsy
+ * @Description:
+ * @Date: Create in 15:10 2020/3/22
+ */
+public class profileController {
+}
---
+++ src/main/java/net/bill99/community/community/interceptor/SessionInterceptor.java
@@ -0,0 +1,9 @@
+package net.bill99.community.community.interceptor;
+
+/**
+ * @Author: wsy
+ * @Description:
+ * @Date: Create in 22:16 2020/3/22
+ */
+public class SessionInterceptor {
+}
